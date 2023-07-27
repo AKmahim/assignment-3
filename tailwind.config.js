cbb5c820be3140cb9a7ba2392ce9f61baba0df3d
AKmahim <mdmahimhossen134@gmail.com>
2023-07-27 17:16:23 +0600

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,69 @@
+// module.exports = {
+//     //...
+//     daisyui: {
+//       themes: ["light", "dark", "cupcake"],
+//     },
+//   }
+
 module.exports = {
-    //...
-    daisyui: {
-      themes: ["light", "dark", "cupcake"],
-    },
+    theme: {
+      screens: {
+        
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+        'xsm': {'max': '576px'},
+        // => @media (max-width: 576px) { ... }
+        '2xsm': {'max': '450px'},
+        // => @media (max-width: 450p) { ... }
+        '3xsm': {'max': '375px'},
+        // => @media (max-width: 375px) { ... }
+        '4xsm': {'max': '320px'},
+        // => @media (max-width: 320px) { ... }
+      }
+    }
   }
+{/* <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+            screens: {
+        
+                '2xl': {'max': '1535px'},
+                // => @media (max-width: 1535px) { ... }
+          
+                'xl': {'max': '1279px'},
+                // => @media (max-width: 1279px) { ... }
+          
+                'lg': {'max': '1023px'},
+                // => @media (max-width: 1023px) { ... }
+          
+                'md': {'max': '767px'},
+                // => @media (max-width: 767px) { ... }
+          
+                'sm': {'max': '639px'},
+                // => @media (max-width: 639px) { ... }
+                'xsm': {'max': '576px'},
+                // => @media (max-width: 576px) { ... }
+                '2xsm': {'max': '450px'},
+                // => @media (max-width: 450p) { ... }
+                '3xsm': {'max': '375px'},
+                // => @media (max-width: 375px) { ... }
+                '4xsm': {'max': '320px'},
+                // => @media (max-width: 320px) { ... }
+              }
+        }
+      }
+    }
+  </script> */}
